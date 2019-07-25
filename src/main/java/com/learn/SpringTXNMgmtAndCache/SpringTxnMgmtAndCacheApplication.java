d2e2c06ebac0b9567d4d13020ae2e38b39c03ac3
Dokku <vsdokku@cgifederal.com>
2019-07-25 19:34:17 -0400

    Later COmmit

---
+++ src/main/java/com/learn/SpringTXNMgmtAndCache/SpringTxnMgmtAndCacheApplication.java
@@ -0,0 +1,13 @@
+package com.learn.SpringTXNMgmtAndCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxnMgmtAndCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTxnMgmtAndCacheApplication.class, args);
+	}
+
+}
